2c9e01117e8559e332a5c1989a2a10576fd1c49e
Vihanga Ashinshana <96816258+vihangayt0@users.noreply.github.com>
2023-04-22 22:49:50 +0530

    💫 Astro-MD V8 Public Relese 👨‍🚀🚀
    💫 Astro-MD V8 Public Relese 👨‍🚀🚀

--- config.js
+++ config.js
@@ -1,19 +1,14 @@
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+/*
+* @project_name AstroMD [WA Multi-device]
+* @author VihangaYT <https://github.com/vihangayt0>
+* @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
+* @link <https://github.com/vihangayt0/Astro-MD-V8>
+* @version 8.0.0
+© 2023 Astro MD, VihangaYT. All rights reserved.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+*/
 
+const _0x8938e6=_0x51fc;(function(_0x44e49f,_0x2bcc91){const _0x370f56={_0x38d157:0x1dd,_0x3a18e6:0x1cc,_0x19e662:0x1cd,_0x5bea24:0x1d0,_0x38b74f:0x1cf,_0x39c759:0x1ce},_0x3af0a6=_0x51fc,_0x46ea6f=_0x44e49f();while(!![]){try{const _0x232b7d=-parseInt(_0x3af0a6(0x1d8))/(-0x101+0x1b*0xab+-0x1*0x1107)+-parseInt(_0x3af0a6(0x1de))/(0x2b*-0x8d+0x172b+0x86)+parseInt(_0x3af0a6(_0x370f56._0x38d157))/(0x5*0x2e3+0x1daf+0x1*-0x2c1b)*(parseInt(_0x3af0a6(_0x370f56._0x3a18e6))/(0x1*0x1189+0x281+0x1d2*-0xb))+parseInt(_0x3af0a6(0x1d9))/(0x7*-0x10f+0x25*0xa7+0xd*-0x149)+-parseInt(_0x3af0a6(_0x370f56._0x19e662))/(0x1c52+0x1*-0x22ed+0x6a1*0x1)*(parseInt(_0x3af0a6(_0x370f56._0x5bea24))/(-0x1*-0x805+-0xbd8*-0x2+-0x1fae))+parseInt(_0x3af0a6(0x1df))/(0x1555+0x1*0x463+0x4*-0x66c)+parseInt(_0x3af0a6(_0x370f56._0x38b74f))/(0xb56*-0x3+-0x1f49+0x4154)*(parseInt(_0x3af0a6(_0x370f56._0x39c759))/(0x1e9*0x3+0x10da*0x2+-0x5*0x7e1));if(_0x232b7d===_0x2bcc91)break;else _0x46ea6f['push'](_0x46ea6f['shift']());}catch(_0xd40414){_0x46ea6f['push'](_0x46ea6f['shift']());}}}(_0x7029,0x19a452+0x8*-0x662b+0x1*-0x8fb13));const {Sequelize}=require(_0x8938e6(0x1da)),fs=require('\x66\x73'),_0x8e55b={};_0x8e55b[_0x8938e6(0x1d6)]=_0x8938e6(0x1d4)+'\x6e\x76';if(fs['\x65\x78\x69\x73\x74\x73\x53\x79\x6e\x63'](_0x8938e6(0x1e1)))require(_0x8938e6(0x1e0))[_0x8938e6(0x1db)](_0x8e55b);function _0x7029(){const _0x5112c3=['\x38\x30\x34\x39\x39\x72\x70\x5a\x4b\x6f\x6a','\x32\x38\x37\x31\x33\x31\x36\x6a\x77\x48\x62\x46\x52','\x36\x30\x37\x38\x34\x36\x34\x74\x6a\x41\x4b\x54\x67','\x64\x6f\x74\x65\x6e\x76','\x63\x6f\x6e\x66\x69\x67\x2e\x65\x6e\x76','\x32\x31\x32\x72\x48\x72\x6c\x4b\x75','\x36\x31\x36\x35\x35\x38\x32\x4d\x54\x65\x48\x4e\x4a','\x31\x33\x37\x30\x31\x30\x6a\x76\x56\x79\x6e\x4b','\x35\x30\x34\x4b\x52\x6f\x73\x69\x4b','\x37\x73\x57\x55\x68\x52\x56','\x74\x72\x75\x65','\x44\x42\x5f\x55\x52\x4c','\x47\x41\x49\x4d\x72','\x2e\x2f\x63\x6f\x6e\x66\x69\x67\x2e\x65','\x65\x6e\x76','\x70\x61\x74\x68','\x65\x78\x70\x6f\x72\x74\x73','\x37\x36\x31\x31\x37\x33\x47\x6b\x64\x4a\x53\x76','\x35\x37\x38\x39\x33\x39\x30\x77\x69\x6e\x5a\x4e\x64','\x73\x65\x71\x75\x65\x6c\x69\x7a\x65','\x63\x6f\x6e\x66\x69\x67','\x53\x45\x53\x53\x49\x4f\x4e\x5f\x49\x44'];_0x7029=function(){return _0x5112c3;};return _0x7029();}function convertToBool(_0x254fd0,_0x401a68=_0x8938e6(0x1d1)){const _0x33b4c5={_0x27f6fa:0x1d3},_0x2ff3da=_0x8938e6,_0x3fdf95={};_0x3fdf95['\x47\x41\x49\x4d\x72']=function(_0x15267d,_0x21c0f3){return _0x15267d===_0x21c0f3;};const _0x107fd0=_0x3fdf95;return _0x107fd0[_0x2ff3da(_0x33b4c5._0x27f6fa)](_0x254fd0,_0x401a68)?!![]:![];}function _0x51fc(_0x402500,_0x442282){const _0x378fd6=_0x7029();return _0x51fc=function(_0x2fb378,_0x5e10f0){_0x2fb378=_0x2fb378-(-0x11d1+-0xf89+0x1193*0x2);let _0x33bb96=_0x378fd6[_0x2fb378];return _0x33bb96;},_0x51fc(_0x402500,_0x442282);}const _0x47b9ac={};_0x47b9ac[_0x8938e6(0x1dc)]=process['\x65\x6e\x76'][_0x8938e6(0x1dc)]===undefined?'':process[_0x8938e6(0x1d5)]['\x53\x45\x53\x53\x49\x4f\x4e\x5f\x49\x44'],_0x47b9ac[_0x8938e6(0x1d2)]=process[_0x8938e6(0x1d5)][_0x8938e6(0x1d2)]===undefined?'':process[_0x8938e6(0x1d5)]['\x44\x42\x5f\x55\x52\x4c'],module[_0x8938e6(0x1d7)]=_0x47b9ac;
 
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-module.exports = {
-ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
-LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
-ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
-CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
-INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
-HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
-};
+// Powered By VihangaYT
